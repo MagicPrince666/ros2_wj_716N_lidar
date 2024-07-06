274c52df12b0e77960b6c618e6f648b1775bc71d
Leo <846863428@qq.com>
2024-07-06 14:38:56 +0000

    暂存

---
+++ src/wj_716N_lidar_01.cpp
@@ -0,0 +1,117 @@
+#if defined(USE_ROS_NORTIC_VERSION) || defined(USE_ROS_MELODIC_VERSION)
+#include "ros/ros.h"
+#else
+#include "rclcpp/rclcpp.hpp"
+#endif
+#include "async_client.h"
+#include "wj_716N_lidar_protocol.h"
+using namespace wj_lidar;
+
+/* ------------------------------------------------------------------------------------------
+ *  show demo --
+ * ------------------------------------------------------------------------------------------ */
+wj_716N_lidar_protocol *protocol;
+Async_Client *client;
+
+void callback(wj_716N_lidar::wj_716N_lidarConfig &config,uint32_t level)
+{
+    protocol->setConfig(config,level);
+}
+
+int main(int argc, char **argv)
+{
+#if defined(USE_ROS_NORTIC_VERSION) || defined(USE_ROS_MELODIC_VERSION)
+    ros::init(argc, argv, "wj_716N_lidar_01");
+    auto node = std::make_shared<ros::NodeHandle>("~");
+    
+    std::string hostname;
+    node->getParam("hostname", hostname);
+    std::string port;
+    node->getParam("port", port);
+    cout << "laser ip: " << hostname << ", port:" << port <<endl;
+
+    auto protocol = std::make_shared<wj_716N_lidar_protocol>(node);
+    dynamic_reconfigure::Server<wj_716N_lidar::wj_716N_lidarConfig> server;
+    dynamic_reconfigure::Server<wj_716N_lidar::wj_716N_lidarConfig>::CallbackType f;
+    f = boost::bind(&callback,_1,_2);
+    server.setCallback(f);
+
+    client = new Async_Client(protocol.get());
+    protocol->heartstate = false;
+    while(!client->m_bConnected) {
+        ROS_INFO("Start connecting laser!");
+        if(client->connect(hostname.c_str(),atoi(port.c_str()))) {
+            ROS_INFO("Succesfully connected. Hello wj_716N_lidar!");
+        } else {
+            ROS_INFO("Failed to connect to laser. Waiting 5s to reconnect!");
+        }
+        ros::Duration(5).sleep();
+    }
+    while(ros::ok()) {
+        ros::spinOnce();
+        ros::Duration(2).sleep();
+        if(client->m_bConnected) {
+            if(protocol->heartstate) {
+                protocol->heartstate = false;
+            } else {
+                client->m_bConnected = false;
+            }
+        } else {
+            //reconnect
+            if(!client->m_bReconnecting) {
+                boost::thread t(boost::bind(&Async_Client::reconnect, client));
+            }
+        }
+    }
+    ros::shutdown();
+    
+#else
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("wj_716N_lidar_01");
+    std::string hostname;
+    std::string port;
+    // declare ros2 param
+    node->declare_parameter<std::string>("hostname", hostname);
+    node->declare_parameter<std::string>("port", port);
+
+    // get ros2 param
+    node->get_parameter("hostname", hostname);
+    node->get_parameter("port", port);
+    cout << "laser ip: " << hostname << ", port:" << port <<endl;
+    std::unique_ptr<wj_716N_lidar_protocol> protocol(new wj_716N_lidar_protocol(node));
+
+    client = new Async_Client(protocol.get());
+    protocol->heartstate = false;
+    rclcpp::Rate loop_5s_rate(5);
+    while(!client->m_bConnected) {
+        RCLCPP_INFO(node->get_logger(), "Start connecting laser!");
+        if(client->connect(hostname.c_str(),atoi(port.c_str()))) {
+            RCLCPP_INFO(node->get_logger(), "Succesfully connected. Hello wj_716N_lidar!");
+        } else {
+            RCLCPP_INFO(node->get_logger(), "Failed to connect to laser. Waiting 5s to reconnect!");
+        }
+        loop_5s_rate.sleep();
+    }
+
+    rclcpp::Rate loop_rate(2);
+    while(rclcpp::ok()) {
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+        if(client->m_bConnected) {
+            if(protocol->heartstate) {
+                protocol->heartstate = false;
+            } else {
+                client->m_bConnected = false;
+            }
+        } else {
+            //reconnect
+            if(!client->m_bReconnecting) {
+                boost::thread t(boost::bind(&Async_Client::reconnect, client));
+            }
+        }
+    }
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+#endif
+}
---
+++ src/wj_716N_lidar_protocol.h
@@ -0,0 +1,75 @@
+#ifndef WJ_716N_LIDAR_PROTOCOL_H
+#define WJ_716N_LIDAR_PROTOCOL_H
+#include <iostream>
+#include "string.h"
+#include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/placeholders.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/bind/bind.hpp>
+#if defined(USE_ROS_NORTIC_VERSION) || defined(USE_ROS_MELODIC_VERSION)
+#include "ros/ros.h"
+#include <sensor_msgs/LaserScan.h>
+using LaserScanMsg = sensor_msgs::LaserScan;
+#else
+#include "rclcpp/rclcpp.hpp"
+#include <sensor_msgs/msg/laser_scan.hpp>
+using LaserScanMsg = sensor_msgs::msg::LaserScan;
+#endif
+
+#include <wj_716N_lidar/wj_716N_lidarConfig.h>
+
+using namespace std ;
+namespace wj_lidar
+{
+#define MAX_LENGTH_DATA_PROCESS 200000
+typedef struct TagDataCache
+{
+    unsigned char m_acdata[MAX_LENGTH_DATA_PROCESS];
+    unsigned int m_u32in;
+    unsigned int m_u32out;
+}DataCache;
+
+class wj_716N_lidar_protocol
+{
+public:
+#if defined(USE_ROS_NORTIC_VERSION) || defined(USE_ROS_MELODIC_VERSION)
+    wj_716N_lidar_protocol(std::shared_ptr<ros::NodeHandle> node);
+#else
+    wj_716N_lidar_protocol(std::shared_ptr<rclcpp::Node> node);
+#endif
+    ~wj_716N_lidar_protocol();
+    bool dataProcess(unsigned char *data,const int reclen);
+    bool protocl(unsigned char *data,const int len);
+    bool OnRecvProcess(unsigned char *data, int len);
+    bool checkXor(unsigned char *recvbuf, int recvlen);
+    void send_scan(const char *data,const int len);
+
+#if defined(USE_ROS_NORTIC_VERSION) || defined(USE_ROS_MELODIC_VERSION)
+    std::shared_ptr<ros::NodeHandle> ros_node_;
+    std::shared_ptr<ros::Publisher> marker_pub_;
+#else
+    std::shared_ptr<rclcpp::Node> ros_node_;
+    rclcpp::Publisher<ImuMsg>::SharedPtr marker_pub_;
+    rclcpp::TimerBase::SharedPtr imu_timer_;
+#endif
+    LaserScanMsg scan;
+    bool setConfig(wj_716N_lidar::wj_716N_lidarConfig &new_config,uint32_t level);
+    bool heartstate;
+private:
+    void movedata(DataCache &sdata);
+    DataCache   m_sdata;
+    wj_716N_lidar::wj_716N_lidarConfig config_;
+    unsigned int m_u32PreFrameNo;
+    unsigned int m_u32ExpectedPackageNo;
+    int m_n32currentDataNo;
+    float scandata[1081];
+    float scanintensity[1081];
+    int total_point;
+    int index_start;
+    int index_end;
+    int freq_scan;
+};
+
+}
+#endif // WJ_716N_LIDAR_PROTOCOL_H
