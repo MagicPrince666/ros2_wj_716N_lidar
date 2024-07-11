e31bc90b3091eccdd75d4e4debf28c2f0ed20a7a
MagicPrince666 <846863428@qq.com>
2024-07-11 23:24:47 +0800

    update code

--- src/async_client.cpp
+++ src/async_client.cpp
@@ -6,7 +6,7 @@ Async_Client::Async_Client(wj_716N_lidar_protocol *protocol)
     m_bConnected = false;
     m_bReconnecting = false;
     m_pSocket = std::shared_ptr<boost::asio::ip::tcp::socket>(new boost::asio::ip::tcp::socket(m_io));
-    cout << "TCP-Connection is initialized!" << endl;
+    std::cout << "TCP-Connection is initialized!" << std::endl;
 }
 
 Async_Client::~Async_Client()
@@ -52,7 +52,7 @@ bool Async_Client::disconnect()
 {
     try
     {
-        cout << "Disconnecting connection!" << endl;
+        std::cout << "Disconnecting connection!" << std::endl;
         m_bConnected = false;
         if(m_pSocket->is_open()) {
             boost::system::error_code errorcode;
@@ -90,16 +90,16 @@ void Async_Client::reconnect()
             boost::system::error_code errorcode;
             m_pSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both, errorcode);
             m_pSocket->close();
-            cout << "Initializing network!" << endl;
+            std::cout << "Initializing network!" << std::endl;
         }
         sleep(3);
-        cout << "Start reconnecting laser!" << endl;
+        std::cout << "Start reconnecting laser!" << std::endl;
         sleep(2);
         if (connect(m_sServerIp, m_iServerPort)) {
-            cout << "Succesfully connected!" << endl;
+            std::cout << "Succesfully connected!" << std::endl;
             break;
         } else {
-            cout << "Failed to reconnect!" << endl;
+            std::cout << "Failed to reconnect!" << std::endl;
         }
         sleep(2);
     }
